a371b52265c10c666932438da7d1cd74ade82f5d
mostaf7583 <mostaf7583@gmail.com>
2022-05-09 17:52:17 +0200

    nothing

--- OS milestone1/src/main.java
+++ OS milestone1/src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
+        
         Interpter i = new Interpter();
-        System.out.print(i.readFile("program.txt"));
+        System.out.println("mohamed");
     }
 }
