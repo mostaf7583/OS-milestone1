6029b868dc16747634b2647ab94d76d62e5934ca
seifahmed74 <95249784+seifahmed74@users.noreply.github.com>
2022-05-10 21:01:10 +0200

    hallo

--- OS milestone1/src/Scheduler.java
+++ OS milestone1/src/Scheduler.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Scheduler {
-    
+    Queue<String> blocked = new LinkedList<String>();
+    Queue<String> ready = new LinkedList<String>();
+
 }
