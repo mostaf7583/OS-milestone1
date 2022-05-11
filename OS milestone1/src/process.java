536fd5e8737364774eef4ce7bbd586f9696b835d
seifahmed74 <95249784+seifahmed74@users.noreply.github.com>
2022-05-11 15:37:55 +0200

    yes

--- OS milestone1/src/process.java
+++ OS milestone1/src/process.java
@@ -1,59 +1,41 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class process {
-	private int pid;
-	private int timeOfArrival;
-	private ArrayList<String[]> instructions = new ArrayList<>();
-	private int PC = 0;
-	private HashMap<String, String> variables = new HashMap<String, String>();
+    private int pid;
+    private state process_State;
+    private 
+    private int PC;
+    private HashMap<String,String>variables= new HashMap<String,String>();
+    
+    
+    public HashMap<String, String> getVariables() {
+        return variables;
+    }
+    public void setVariables(HashMap<String, String> variables) {
+        this.variables = variables;
+    }
 
-	
-	
-	public process(int pid,int timeOfArrival) {
-		this.pid = pid;
-		this.timeOfArrival = timeOfArrival;
-	}
+    public int getPid() {
+        return pid;
+    }
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+    public int getNo_of_instructions() {
+        return no_of_instructions;
+    }
+    public void setNo_of_instructions(int no_of_instructions) {
+        this.no_of_instructions = no_of_instructions;
+    }
+    public state getProcess_State() {
+        return process_State;
+    }
+    public void setProcess_State(state process_State) {
+        this.process_State = process_State;
+    }
+    public int getPC() {
+        return PC;
+    }
 
-	public HashMap<String, String> getVariables() {
-		return variables;
-	}
-
-	public void setVariables(HashMap<String, String> variables) {
-		this.variables = variables;
-	}
-
-	public int getPid() {
-		return pid;
-	}
-
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
-
-	public int getPC() {
-		return PC;
-	}
-
-	public ArrayList<String[]> getInstructions() {
-		return instructions;
-	}
-
-	public void setInstructions(ArrayList<String[]> instructions) {
-		this.instructions = instructions;
-	}
-
-	public int getTimeOfArrival() {
-		return timeOfArrival;
-	}
-
-	public void setTimeOfArrival(int timeOfArrival) {
-		this.timeOfArrival = timeOfArrival;
-	}
-	
-	
-	public void addToVariables(String key,String value) {
-		this.variables.put(key, value);
-	}
-
+    
 }
